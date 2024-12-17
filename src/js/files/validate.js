8475023995508335bfea950c95b29fe9f9777a7c
pauchok-sk <pauchoooooook@gmail.com>
2024-12-18 01:40:55 +0300

    connection

--- src/js/files/validate.js
+++ src/js/files/validate.js
@@ -3,27 +3,57 @@ import JustValidate from "just-validate";
 export default function validate() {
   const modalFormCall = new JustValidate("#form-order-call");
 
-  modalFormCall
-    .addField("#order-call-name", [
-      {
-        rule: "minLength",
-        value: 3,
-        errorMessage: "Минимум 3 символа",
-      },
-      {
-        rule: "required",
-        errorMessage: "Это поле должно быть заполнено",
-      },
-    ])
-    .addField("#order-call-tel", [
-      {
-        rule: "minLength",
-        value: 3,
-        errorMessage: "Минимум 3 символа",
-      },
-      {
-        rule: "required",
-        errorMessage: "Это поле должно быть заполнено",
-      },
-    ]);
+  if (modalFormCall) {
+    modalFormCall
+      .addField("#order-call-name", [
+        {
+          rule: "minLength",
+          value: 3,
+          errorMessage: "Минимум 3 символа",
+        },
+        {
+          rule: "required",
+          errorMessage: "Это поле должно быть заполнено",
+        },
+      ])
+      .addField("#order-call-tel", [
+        {
+          rule: "minLength",
+          value: 3,
+          errorMessage: "Минимум 3 символа",
+        },
+        {
+          rule: "required",
+          errorMessage: "Это поле должно быть заполнено",
+        },
+      ]);
+  }
+
+  const formConnection = new JustValidate("#form-connection");
+
+  if (formConnection) {
+    formConnection
+      .addField("#connection-name", [
+        {
+          rule: "minLength",
+          value: 3,
+          errorMessage: "Минимум 3 символа",
+        },
+        {
+          rule: "required",
+          errorMessage: "Это поле должно быть заполнено",
+        },
+      ])
+      .addField("#connection-tel", [
+        {
+          rule: "minLength",
+          value: 3,
+          errorMessage: "Минимум 3 символа",
+        },
+        {
+          rule: "required",
+          errorMessage: "Это поле должно быть заполнено",
+        },
+      ]);
+  }
 }
